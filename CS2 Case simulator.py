d1b55f66dd2ee9a7a0d0234c0836c96079d7cc86
Fedoorotikus <161240784+Fedoorotikus@users.noreply.github.com>
2025-02-11 15:44:34 +0300

    Update CS2 Case simulator
     + Added Revolution case
     - Removed float generation (Possibly will be readded after reworks)

--- CS2 Case simulator.py
+++ CS2 Case simulator.py
@@ -2,7 +2,6 @@ from random import randint
 import os
 def opencase(q):
     c=0
-    fl=randint(0,100_000_000)/100_000_000
     st=''
     skin=''
     patt=''
@@ -62,15 +61,12 @@ def opencase(q):
     skin=st+skins[rar][randint(0,len(skins[rar])-3)]+' ' + skins[rar][-2]
     print(skin)
     print('pattern '+ patt)
-    print('float ', str(fl))
     f.write(skin+'\n')
     f.write('pattern '+ patt+'\n')
-    f.write('float '+ str(fl)+'\n')
     f.write('--------------------------------------\n')
     f.close()
 def Dlore(q):
     c=0
-    fl=randint(0,100_000_000)/100_000_000
     st=''
     skin=''
     patt=''
@@ -97,10 +93,65 @@ def Dlore(q):
     skin=st+skins[rar][randint(0,len(skins[rar])-3)]+' ' + skins[rar][-2]
     print(skin)
     print('pattern '+ patt)
-    print('float ', str(fl))
     f.write(skin+'\n')
     f.write('pattern '+ patt+'\n')
-    f.write('float '+ str(fl)+'\n')
+    f.write('--------------------------------------\n')
+    f.close()
+def Revolution(q):
+    c=0
+    st=''
+    skin=''
+    patt=''
+    wait=15
+    rar=0
+    skins = [['SG553 | Cyberforce','SCAR-20 | Fragments','P250 | Re.built','Tec-9 | Rebel','MAG-7 | Insomnia', 'MP-5SD | Liquidation', 'MP9 | Featherweight','(Mil-spec grade)','1'],
+             ['MAC-10 | Sakkaku','P90 | Neoqueen', 'Glock-18 | Umbral Rabbit', 'Revolver R8 | Banana Cannon', 'M4A1-S | Emphorosaur - S','(Restricted grade)','5'],
+             ['UMP-45 | Wild Child','AWP | Duality','P2000 | Wicked Sick','(Classified grade)','D'],
+             ['AK-47 | Head Shot','M4A4 | Temukau','(Covert grade)','4'],
+             ['Sport Gloves | Vice','Specialist Gloves | Fade','Hand Wraps | Cobalt Skulls','Specialist Gloves | Crimson Web','Sport Gloves | Amphibious','Driver Gloves | Imperial Plaid','Driver Gloves | King Snake','Moto Gloves | POW!','Specialist Gloves | Mogul','Sport Gloves | Omega','Moto Gloves | Polygon','Hand Wraps | Overprint','Moto Gloves | Turtle', 'Sport Gloves | Bronze Morph','Driver Gloves | Overtake','Hydra Gloves | Case Hardened','Hand Wraps | Duct Tape','Hydra Gloves | Emerald','Hand Wraps | Arboreal', 'Moto Gloves | Transport','Specialist Gloves | Buckshot','Driver Gloves | Racing Green','Hydra Gloves | Rattler','Hydra Gloves | Mangrove','(Special)','6']]
+    rar=randint(1,4)
+    if rar == 2:
+        st = 'Stattrack '
+    rar=randint(0,500)
+    if q!='q':
+        for i in range(23):
+            rar=randint(0,500)
+            wait+=wait
+            os.system('cls')
+            if rar<=399:
+                rar=0
+            elif rar<=459:
+                rar=1
+            elif rar<=489:
+                rar=2
+            elif rar<=499:
+                rar=3
+            else:
+                rar=4
+            os.system('color '+skins[rar][-1])
+            print(skins[rar][randint(0,len(skins[rar])-3)]+' ' + skins[rar][-2])
+            for i in range(wait+10_000_000):
+                pass
+    os.system('cls')
+    if rar<=399:
+        rar=0
+    elif rar<=459:
+        rar=1
+    elif rar<=489:
+        rar=2
+    elif rar<=499:
+        rar=3
+    else:
+        rar=4
+    if rar == 4:
+        st = ''
+    os.system('color '+skins[rar][-1])
+    patt=str(randint(0,1000))
+    skin=st+skins[rar][randint(0,len(skins[rar])-3)]+' ' + skins[rar][-2]
+    print(skin)
+    print('pattern '+ patt)
+    f.write(skin+'\n')
+    f.write('pattern '+ patt+'\n')
     f.write('--------------------------------------\n')
     f.close()
 chosen = 'Simulator case'
@@ -114,11 +165,14 @@ while True:
         print("Options:")
         print('Simulator case(original case) - "sim"')
         print('Dragon Lore case(original case) - "dlore"')
+        print('Revolution case - "rev"')
         inp = input()
         if inp == 'sim':
             chosen = 'Simulator case'
         elif inp == 'dlore':
             chosen = 'Dragon Lore case'
+        elif inp == 'rev':
+            chosen = 'Revolution case'
         else:
             print('This is not an available option.')
         os.system('cls')
@@ -127,3 +181,5 @@ while True:
             opencase(inp)
         elif chosen == 'Dragon Lore case':
             Dlore(inp)
+        elif chosen == 'Revolution case':
+            Revolution(inp)
